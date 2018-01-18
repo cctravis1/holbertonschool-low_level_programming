1d3101bd6014d13fb5756f003f04fcddb304f69c
cctravis1 <cctravis@gmail.com>
2018-01-18 02:45:11 +0000

    removed extras

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,17 +6,11 @@
  */
 int main(void)
 {
-	char charType;
-	int integerType;
-	long int longintType;
-	long long int longlngintType;
-	float floatType;
-
-	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
-	printf("Size of a int: %ld byte(s)\n", sizeof(integerType));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(longintType));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(longlngintType));
-	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of a int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 
 	return (0);
 }
