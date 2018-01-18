ff47b1eb41cbbf906eb3e533b6785cf6037caa1d
cctravis1 <cctravis@gmail.com>
2018-01-18 02:38:47 +0000

    space after commas

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long long int longlngintType;
 	float floatType;
 
-	printf("Size of a char: %ld byte(s)\n",sizeof(charType));
-	printf("Size of a int: %ld byte(s)\n",sizeof(integerType));
-	printf("Size of a long int: %ld byte(s)\n",sizeof(longintType));
-	printf("Size of a long long int: %ld byte(s)\n",sizeof(longlngintType));
-	printf("Size of a float: %ld byte(s)\n",sizeof(floatType));
+	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+	printf("Size of a int: %ld byte(s)\n", sizeof(integerType));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(longlngintType));
+	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 
 	return (0);
 }
