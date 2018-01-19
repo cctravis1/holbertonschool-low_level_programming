afd024f3ec515a53634699c90f7912c864b5a07a
cctravis1 <cctravis@gmail.com>
2018-01-19 01:57:42 +0000

    added greater then or equal 6

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	l = n % 10;
-	if (n > 5)
+	if (n >= 6)
 		printf("Last digit of %d is %i and is greater than 5\n", n, l);
 	else if (n < 6 && n != 0)
 		printf("Last digit of %d is %i and is less than 6 and not 0\n",
