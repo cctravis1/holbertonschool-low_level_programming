f10aa133bccbbf49861b0f75c1727effcee33c07
cctravis1 <cctravis@gmail.com>
2018-01-19 22:55:51 +0000

    first try

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int c;
+	int k;
+
+	for (i = '0' ; i <= '7' ; i++)
+	{
+		for (c = i + 1; c <= '8'; c++)
+		{
+			for (k = c + 1; k <= '9'; k++)
+			{
+				putchar(i);
+				putchar(c);
+				putchar(k);
+				if(i < '7' || c < '8' || k < '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	{ putchar('\n');
+	}
+	return (0);
+}
