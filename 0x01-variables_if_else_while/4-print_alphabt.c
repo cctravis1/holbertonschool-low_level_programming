fb81738380e22b8b5c47c8776afd869bb4918856
cctravis1 <cctravis@gmail.com>
2018-01-19 03:56:44 +0000

    try again

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,14 +10,14 @@ int main(void)
 	char ch;
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
-{
+	{
 		if (ch == 'e')
-{
+		{
 			ch = ch + 1;
 			continue;
 		}
 		if (ch == 'q')
-{
+		{
 			ch = ch + 1;
 			continue;
 		}
