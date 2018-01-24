cede9260835a87e2d5a05cbacf8570421e4410cb
cctravis1 <cctravis@gmail.com>
2018-01-24 03:22:12 +0000

    first try

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - check the code for Holberton School students.
+ * @c: - first
+ *
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
