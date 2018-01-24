539785c02b49f67da46d6d32619ee45864dd5c7b
cctravis1 <cctravis@gmail.com>
2018-01-24 01:04:04 +0000

    first try

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+
+{
+	int y = 0;
+
+	while (y <= 10)
+	{
+		int x = 'a';
+		while (x <= 'z')
+		{
+		_putchar(x);
+		x++;
+		}
+		_putchar('\n');
+		y++;
+	}
+}
