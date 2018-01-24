23a44a0bbe54f5bb739724dd9361017fa6819d0d
cctravis1 <cctravis@gmail.com>
2018-01-24 04:40:50 +0000

    first try

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _abs - check the code for Holberton School students.
+ * @x: - first
+ *
+ * Return: Always 0.
+ */
+
+int _abs(x)
+{
+	int y;
+
+	y = (x >> 31);
+
+	return ((x ^ y) - y);
+}
