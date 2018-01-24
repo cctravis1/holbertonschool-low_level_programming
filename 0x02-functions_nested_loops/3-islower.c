5d3f27e066edbf8c5366c47c0084472cfe89b016
cctravis1 <cctravis@gmail.com>
2018-01-24 02:04:04 +0000

    first try

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _islower - check the code for Holberton School students.
+ * @c: - first
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+		return (1);
+			}
+	else
+	{
+		return (0);
+			}
+}
