bbaf99d18e666ced1ebf2f50ad354a15f7ae4d55
cctravis1 <cctravis@gmail.com>
2018-01-25 01:07:57 +0000

    first try

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - check the code for Holberton School students.
+ * @n: first
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+}
