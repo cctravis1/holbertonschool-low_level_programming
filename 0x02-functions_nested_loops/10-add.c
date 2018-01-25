c7681ffc88d596d00a90f79b309aae8130f108a8
cctravis1 <cctravis@gmail.com>
2018-01-25 00:37:59 +0000

    first try

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * add - check the code for Holberton School students.
+ * @a: first
+ * @b: second
+ * Return: add
+ *
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
