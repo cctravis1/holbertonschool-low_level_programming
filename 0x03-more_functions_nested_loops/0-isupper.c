38e8b96384c8b23440daba67aae90a2eb6c884a5
cctravis1 <cctravis@gmail.com>
2018-01-25 22:22:26 +0000

    first add

---
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _isupper - check the code for Holberton School students.
+ * @c: - int of _isupper
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
