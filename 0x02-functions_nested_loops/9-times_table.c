7dcbfe0c4197c5a1db4574a7d86ae49d6d2dfce7
cctravis1 <cctravis@gmail.com>
2018-01-25 00:13:36 +0000

    first try

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * times_table - check the code for Holberton School students.
+ */
+void times_table(void)
+{
+	int r;
+	int x;
+	int s;
+
+	for (r = 0; r <= 9; r++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			s = r * x;
+			if (x == 0)
+			{
+                                _putchar('0');
+			}
+			else if (s <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(s + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(s / 10 + '0');
+				_putchar(s % 10 + '0');
+			}
+		}
+		_putchar ('\n');
+	}
+}
